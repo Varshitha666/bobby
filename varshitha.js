0bcf6bd132ac77452deec9871534d2335ca58d58
Varshitha666 <aalurivarshitha@gmail.com>
2024-06-30 06:11:35 +0530

    Add files via upload

---
+++ varshitha.js
@@ -0,0 +1,7 @@
+var age=22
+        const remainingYears =90-age;
+        const months=remainingYears*12;
+        const weeks=remainingYears*52;
+        const days=remainingYears*365;
+        cosole.log(`your lifespan ${remainingYears}and months${months}and weeks${weeks}and days${days}....`) 
+
